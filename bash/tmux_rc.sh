c091ea6cb0733b39e4613413117ee5c07aa29bd7
kevinkowalew <9099021+kevinkowalew@users.noreply.github.com>
2022-09-08 22:20:59 -0500

    adds kill all sessions alias to tmux_rc

--- bash/tmux_rc.sh
+++ bash/tmux_rc.sh
@@ -6,4 +6,13 @@ function kill_unassigned_sessions() {
 	done
 }
 
+function kill_all_sessions() {
+	SESSIONS=$(tmux ls | cut -d":" -f1)
+	for SESSION in $SESSIONS
+	do
+		tmux kill-session -t $SESSION
+	done
+}
+
 alias tk='kill_unassigned_sessions'
+alias tka='kill_all_sessions'
