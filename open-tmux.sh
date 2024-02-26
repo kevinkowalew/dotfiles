5c8f278a1524ece0f6682509f6acd60e4ee9b05a
kevinkowalew <9099021+kevinkowalew@users.noreply.github.com>
2024-02-25 21:18:38 -0600

    bug fixes for open-tmux script

--- open-tmux.sh
+++ open-tmux.sh
@@ -1,14 +1,12 @@
-cd ~/code
-project=$(find . -mindepth 2 -maxdepth 2 \( -type d -o -type l \) | fzf)
+project=$(find /Users/kevin/code -mindepth 2 -maxdepth 2 \( -type d -o -type l \) | fzf)
 shortName=$(echo $project | rev | cut -d'/' -f1 | rev)
 existing=$(tmux list-windows -F '#{window_index}: #{window_name}'| grep $shortName)
 selected=$(tmux display-message -p '#{window_index}')
-echo Original: $selected
 
 if [ -n "$existing" ]; then
 	id=$(echo $existing | cut -d':' -f1)
 	if [ "$id" -eq $selected ]; then
-		return
+		exit
 	else
 		tmux select-window -t $id
 	fi
@@ -16,4 +14,3 @@ else
 	tmux new-window -c $project -n $shortName 'vim .'
 	selected=$(tmux display-message -p '#{window_index}')
 fi
-cd -
