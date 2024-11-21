561d833acf6a4e0ffb88a8dbd59db06a69477e6e
kevinkowalew <9099021+kevinkowalew@users.noreply.github.com>
2024-11-21 16:09:14 -0600

    adds create to shim script

--- sgit-shim.sh
+++ sgit-shim.sh
@@ -1,4 +1,4 @@
-if [[ $1 == "ls" || $1 == "sync" || $1 == "clone" || $1 == "delete" ]]; then
+if [[ $1 == "ls" || $1 == "create" || $1 == "clone" || $1 == "delete" ]]; then
 	sgit "$@"
 else
 	git "$@"
