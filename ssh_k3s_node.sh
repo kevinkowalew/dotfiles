956a046c5fe5c3321e09d2718cd95f067baba4f5
kevinkowalew <9099021+kevinkowalew@users.noreply.github.com>
2024-12-02 19:43:15 -0600

    updates k3s shh convenience script

--- ssh_k3s_node.sh
+++ ssh_k3s_node.sh
@@ -1,4 +1,3 @@
-NODES=("MASTER" "WORKER_1" "WORKER_2" "WORKER_3" "WORKER_4")
-ENV_VAR=$(printf "%s\n" "${NODES[@]}" | fzf) 
-IP=$(env | grep $ENV_VAR | head -n 1 | cut -d = -f2)
-ssh -i $K3S_SSH_PRIVATE_KEY_PATH $K3S_USER@$IP
+NODES=("master" "worker1" "worker2" "worker3" "worker4")
+NODE=$(printf "%s\n" "${NODES[@]}" | fzf) 
+ssh -i $K3S_SSH_PRIVATE_KEY_PATH $K3S_USER@"${NODE}".local
