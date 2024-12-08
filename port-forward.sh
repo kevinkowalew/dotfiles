8cfa74a0fc50ec3fcaa85d1e2b3c90f8772eb052
kevinkowalew <9099021+kevinkowalew@users.noreply.github.com>
2024-12-08 10:15:20 -0600

    adds -o flag to port-forward.sh script

--- port-forward.sh
+++ port-forward.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
+flag_c=false
+
+for arg in "$@"; do
+  if [[ "$arg" == "-o" ]]; then
+    flag_c=true
+  fi
+done
+
 TARGET=$(kubectl get svc | grep -v NAME | fzf)
 NAME=$(echo $TARGET | awk '{print($1)}')
 PORT=$(echo $TARGET | awk '{print($5)}' | cut -d"/" -f1)
 echo $NAME $PORT
 
-open https://localhost:$PORT && kubectl port-forward svc/$NAME $PORT:$PORT
+if $flag_c; then
+	open https://localhost:$PORT
+fi
+
+kubectl port-forward svc/$NAME $PORT:$PORT
