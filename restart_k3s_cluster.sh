050580c6a0434dad826bdc38c36fd335c2c79d8d
kevinkowalew <9099021+kevinkowalew@users.noreply.github.com>
2025-10-11 12:50:10 -0500

    adds some logging to restart_k3s_cluster.sh

--- restart_k3s_cluster.sh
+++ restart_k3s_cluster.sh
@@ -2,6 +2,11 @@ nodes=("${MASTER}" "${WORKER_1}" "${WORKER_2}" "${WORKER_3}" "${WORKER_4}")
 
 for node in "${nodes[@]}"; do
 	ssh pi@$node "sudo reboot"
-	echo $?
+	code=$?
+	if (( $code == 0 || $code == 255 )); then
+		echo Successfully restarted $node
+	else
+		echo Failed to restart $node
+	fi
 done
 
